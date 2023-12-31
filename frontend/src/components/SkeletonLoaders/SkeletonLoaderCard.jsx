c4199eaf9db6dab94175d04db1dcd2382a7a992a
Anish <anishtimalsina747@gmail.com>
2023-12-31 13:20:54 +0545

    Skeleton loader added

---
+++ frontend/src/components/SkeletonLoaders/SkeletonLoaderCard.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const SkeletonLoaderBox = () => {
+    const skeletonArray = Array.from({ length: 4 });
+
+    return (
+        <div>
+            <main className="p-4">
+                <div className="flex flex-wrap">
+                    {skeletonArray.map((_, index) => (
+                        <div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-2">
+                            <div className="rounded-lg shadow-md bg-white dark:bg-gray-800">
+                                <div className="rounded-lg h-48 bg-gray-400 dark:bg-gray-700 animate-pulse" />
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </main>
+        </div>
+    );
+};
+
+const SkeletonLoaderCard = () => {
+    const skeletonArray = Array.from({ length: 6 });
+
+    return (
+        <main className="p-4">
+            <div className="flex flex-wrap">
+                {skeletonArray.map((_, index) => (
+                    <div className="w-full md:w-1/4 lg:w-1/5 xl:w-1/6 p-2">
+                        <div className="rounded-lg shadow-md bg-white dark:bg-gray-800">
+                            <div className="rounded-lg h-48 bg-gray-200 dark:bg-gray-700 animate-pulse" />
+                            <div className="p-4 space-y-2">
+                                <div className="h-4 bg-gray-200 dark:bg-gray-700 animate-pulse" />
+                                <div className="h-3 bg-gray-200 dark:bg-gray-700 w-3/4 animate-pulse" />
+                                <div className="h-3 bg-gray-200 dark:bg-gray-700 w-1/2 animate-pulse" />
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </main>
+    );
+};
+
+const SkeletonLoaderHome = () => {
+    return(
+        <div>
+            <SkeletonLoaderCard />
+            <SkeletonLoaderCard />
+            <SkeletonLoaderBox />
+            <SkeletonLoaderCard />
+            <SkeletonLoaderCard />
+        </div>
+    );
+}
+
+export { SkeletonLoaderCard, SkeletonLoaderBox };
+export default SkeletonLoaderHome;
---
+++ frontend/src/components/SkeletonLoaders/SkeletonLoaderSong.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const SkeletonLoaderSingleSong = () => {
+    return (
+        <div className="mt-4 rounded-md">
+            <div className="flex justify-center items-center space-x-4">
+                <div className="h-16 w-16 rounded-full bg-gray-200 animate-pulse" />
+                <div className="space-y-2">
+                    <div className="h-3 w-[350px] bg-gray-200 animate-pulse" />
+                    <div className="h-3 w-[250px] bg-gray-200 animate-pulse" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const SkeletonLoaderSong = () => {
+    const skeletonArray = Array.from({ length: 10 });
+
+    return (
+        <div>
+            {skeletonArray.map((_, index) => (
+                <SkeletonLoaderSingleSong key={index} />
+            ))}
+        </div>
+    );
+};
+
+
+export default SkeletonLoaderSong;
