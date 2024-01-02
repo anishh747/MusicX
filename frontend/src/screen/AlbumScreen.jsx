a00620db8c8138e4996b6accd7ae6e8f27d20b41
Aditya Dhakal <dhakaladitya96@gmail.com>
2024-01-02 22:11:37 +0545

    album/playlist ui

--- frontend/src/screen/AlbumScreen.jsx
+++ frontend/src/screen/AlbumScreen.jsx
@@ -58,20 +58,59 @@ const AlbumScreen = () => {
         <SkeletonLoaderSong />
       ) : (
         <div>
+
           <div className="max-w-2xl mx-auto px-4">
-            <FaCirclePlay
-              onClick={handleBigPlayButton}
-              className="text-6xl text-green-500 mx-auto hover:cursor-pointer"
-            />
+            <div key="1" className="max-w-md mx-auto rounded-xl  overflow-hidden md:max-w-2xl">
+              <div className="md:flex">
+                <div className="md:flex-shrink-0">
+                  <img
+                    alt="Album cover"
+                    className="h-36 w-full object-cover md:w-36 rounded-full mt-4"
+                    height="300"
+                    src={data.image[1].link}
+                    style={{
+                      aspectRatio: "300/300",
+                      objectFit: "cover",
+                    }}
+                    width="300"
+                  />
+                </div>
+                <div className="p-8 pt-12">
+                  <h2 className="mt-2 text-2xl leading-7 font-bold text-white">{data.name}</h2>
+                  <button className="mt-4 inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-green-600 hover:bg-green-500 focus:outline-none focus:shadow-outline-green focus:border-green-700 active:bg-green-700 transition ease-in-out duration-150 rounded-lg"
+                    onClick={handleBigPlayButton}>
+                    <svg
+                      className="w-4 h-4 mr-2"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                      />
+                      <path
+                        d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                      />
+                    </svg>
+                    Play All
+                  </button>
+                </div>
+              </div>
+            </div>
             <div className="items-start justify-between sm:flex">
-              <h4 className="text-white-800 text-xl py-8 font-semibold">
-                {data.name}
-              </h4>
+
             </div>
             <ul className="album-song-container">
               <li className="album-song-list">
                 <div>
-                  <h3>S.N.</h3>
+                  <h3>S.N</h3>
                   <div className="mx-8">
                     <span>Song Name</span>
                   </div>
@@ -81,7 +120,7 @@ const AlbumScreen = () => {
                 </div>
               </li>
               {data.songs.map((item, idx) => (
-                <Song data={item} _key={idx}/>
+                <Song data={item} _key={idx} />
               ))}
             </ul>
           </div>
